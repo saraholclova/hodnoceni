47727e3040d5a45ba2b6a11199865781a81aeedc
Sára Hölclová <sara.holclova@gmail.com>
2024-05-21 19:22:01 +0200

    komunikace od dítěti k rodiči –> při kliku na hvězdu se zvýší číslo

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -4,16 +4,20 @@ import './style.css';
 
 export const Rating = () => {
   const [rating, setRating] = useState(0);
-  
+
+  const klikNaHvezdu = () => {
+    setRating(rating + 1);
+  };
+
   return (
     <div className="rating">
       <div className="rating__value">{rating}</div>
       <div className="rating__stars">
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-        <Star />
+        <Star onSelect={klikNaHvezdu} />
+        <Star onSelect={klikNaHvezdu} />
+        <Star onSelect={klikNaHvezdu} />
+        <Star onSelect={klikNaHvezdu} />
+        <Star onSelect={klikNaHvezdu} />
       </div>
     </div>
   );
--- src/components/Star/index.jsx
+++ src/components/Star/index.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react';
 import './style.css';
 
-export const Star = () => {
+export const Star = ({ value, onSelect }) => {
   const [glowing, setGlowing] = useState(false);
 
   const starClass = glowing ? 'rating__star rating__star--on' : 'rating__star';
 
-  return <div onClick={() => setGlowing(glowing)} className={starClass}></div>;
+  //onClick={() => setGlowing(!glowing) && onSelect(value)}  NEJDE DÁT DVĚ UDÁLOSTI NA JEDEN KLIK, tím pádem si to musím uložit jako proměnnou a tu pak vloži na onClick NEASI
+
+  const handleSelect = () => {
+    onSelect(value);
+    setGlowing(!glowing);
+  };
+
+  return (
+    <div onClick={handleSelect} className={starClass}>
+      {/* {value} */}
+    </div>
+  );
 };
