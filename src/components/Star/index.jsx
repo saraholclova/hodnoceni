eb8b37b76807d8c36d55af35e6e8c618e1e73195
Sára Hölclová <sara.holclova@gmail.com>
2024-05-21 18:36:57 +0200

    ternární operátor a stav pro rozsvícení a zhasnutí hvězdy

--- src/components/Star/index.jsx
+++ src/components/Star/index.jsx
@@ -3,12 +3,8 @@ import './style.css';
 
 export const Star = () => {
   const [glowing, setGlowing] = useState(false);
-  
-  const starClass = glowing
-    ? 'rating__star rating__star--on'
-    : 'rating__star';
 
-  return (
-    <div className={starClass}></div>
-  );
+  const starClass = glowing ? 'rating__star rating__star--on' : 'rating__star';
+
+  return <div onClick={() => setGlowing(glowing)} className={starClass}></div>;
 };
